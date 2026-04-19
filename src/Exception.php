b2fea6e0910ba3e0fe61bb1e88eb9dbdf5d11864
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-19 17:16:19 +0200

    feat: Add modern PHP 8.1 / OO implementation

---
+++ src/Exception.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2014-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Idna
+ */
+
+namespace Horde\Idna;
+
+use Horde\Exception\HordeException;
+
+class Exception extends HordeException
+{
+}
